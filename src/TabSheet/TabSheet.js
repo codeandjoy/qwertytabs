79d9cdd618067291ba0f49eb362baf81a19227d3
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-02-13 11:26:23 +0200

    Now registering 'q' key

--- src/TabSheet/TabSheet.js
+++ src/TabSheet/TabSheet.js
@@ -1,34 +1,73 @@
 import TabContent from "./TabContent";
 import "./css/TabSheet.css";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const TabSheet = () => {
-    const [tabContent, ] = useState(
-        {
-            name: "Tab",
-            notes: [
-                [
-                    ["0", "n", "n", "n", "n", "0"],
-                    ["n", "n", "0", "n", "n", "n"],
-                    ["n", "3", "n", "n", "n", "n"],
-                    ["0", "n", "n", "n", "n", "n"],
-                    ["0", "n", "n", "n", "n", "0"]
-                ],
-                [
-                    ["0", "n", "n", "n", "n", "0"],
-                    ["n", "n", "0", "n", "n", "n"],
-                    ["n", "3", "n", "n", "n", "n"],
-                    ["0", "n", "n", "n", "n", "n"],
-                    ["0", "n", "n", "n", "n", "n"]
-                ],
-            ]
+    const [tabState, setTabState] = useState({
+            focusedLine: 1,
+            focusedNoteSet: 4,
+            focusedStrings: [],
+            tabContent: {
+                name: "Tab",
+                notes: [
+                    [
+                        ["0", "n", "n", "n", "n", "0"],
+                        ["n", "n", "0", "n", "n", "n"],
+                        ["n", "3", "n", "n", "n", "n"],
+                        ["0", "n", "n", "n", "n", "n"],
+                        ["0", "n", "n", "n", "n", "0"]
+                    ],
+                    [
+                        ["0", "n", "n", "n", "n", "0"],
+                        ["n", "n", "0", "n", "n", "n"],
+                        ["n", "3", "n", "n", "n", "n"],
+                        ["0", "n", "n", "n", "n", "n"],
+                        ["0", "n", "n", "n", "n", "n"]
+                    ],
+                ]
+            }
         }
     );
 
+    console.log(tabState.focusedStrings);
+
+    // Keyboard shortcuts
+    const handleKeydown = useCallback((event) => {
+        if(event.repeat) return;
+
+        if(event.key === 'q'){
+            console.log("Q down");
+            setTabState((oldState) => { 
+                return { ...oldState, focusedStrings: [ ...oldState.focusedStrings, 0 ] }
+            });
+        }
+    }, []);
+
+    const handleKeyup = useCallback((event) => {
+        if(event.repeat) return;
+        
+        if(event.key === 'q'){
+            console.log("Q up");
+            setTabState((oldState) => {
+                return { ...oldState, focusedStrings: [ ...oldState.focusedStrings.filter(string => string !== 0) ] }
+            });
+        }
+    }, []);
+    
+    useEffect(() => {
+        document.addEventListener('keypress', handleKeydown);
+        document.addEventListener('keyup', handleKeyup);
+        return () => {
+            document.removeEventListener('keydown', handleKeydown);
+            document.removeEventListener('keyup', handleKeyup);
+        }
+    }, [handleKeydown, handleKeyup]);
+    // 
+
     return (
         <div id="TabSheet">
-            <h1 className="tab-name">{ tabContent.name }</h1>
-            <TabContent content={ tabContent }/>
+            <h1 className="tab-name">{ tabState.tabContent.name }</h1>
+            <TabContent content={ tabState.tabContent }/>
         </div>
     );
 }
