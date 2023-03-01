ad721aae9a566991b3b2b391e5fa46aeee0baa1b
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-03-01 08:22:58 +0200

    Now can download TXT tabSheet

--- src/hooks/useDownload.js
+++ src/hooks/useDownload.js
@@ -1,11 +1,74 @@
 import { useContext } from "react";
 import { TabSheetContext } from '../TabSheetContext/TabSheetContext';
 
+const transpose = (matrix) => {
+    const newMatrix = [];
+
+    // Init
+    for(let i = 0; i < 6; i++){
+        newMatrix.push(Array(matrix.length).fill('dummy'));
+    }
+
+    // Transpose
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[0].length; j++){
+            newMatrix[j][i] = matrix[i][j];
+        }
+    }
+
+    return newMatrix;
+}
+
+const paintNotes = (matrix) => {
+    return matrix.map(row  => {
+        return row.map(col => {
+            if(col === 'n') return '---';
+            if(col === 'l') return '|--';
+            if(col.length === 2) return col+'-';
+            return col+'--';
+        })
+    })
+}
+
+const createStringsOfNotes = (matrix) => {
+    const newMatrix = [];
+
+    matrix.forEach(string => {
+        newMatrix.push(string.join(''));
+    });
+
+    return newMatrix;
+}
+
 const useDownload = () => {
     const [tabState,] = useContext(TabSheetContext);
     
     const download = () => {
-        console.log('download');
+        const noteSets = [];
+        const lines = [];
+
+        tabState.tabContent.notes.forEach(line => {
+            line.lineData.forEach(noteSet => {
+                noteSets.push(noteSet.noteSetData);
+            });
+
+            lines.push(createStringsOfNotes(paintNotes(transpose(noteSets))).join('\n'));
+
+            noteSets.length = 0;
+        });
+
+        const tabSheet = lines.join('\n\n');
+        console.log(tabSheet);
+
+        // Download
+        const blob = new Blob([tabSheet], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.download = 'tabsheet.txt';
+        link.href = url;
+        link.click();
+        link.remove();
+        //
     }
 
     return [download];
